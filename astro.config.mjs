c036794dd55e1cf1d44a6ad0366105d2777ff62d
akiogitgit <akimaron2828@icloud.com>
2023-01-31 09:10:06 +0900

    feat: Layout変更、画像追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+// portも変えられる
+// export default defineConfig({})
+
+export default defineConfig({ server: { port: 3000 } })
